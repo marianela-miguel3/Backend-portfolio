2b305b901516cd335e4ea8af4f2debf1a13b471d
marianela miguel <nelaecopol@gmail.com>
2022-11-03 15:05:02 -0300

    cambios 2

--- src/main/java/com/trabajoFinal/app/PortfolioApplication.java
+++ src/main/java/com/trabajoFinal/app/PortfolioApplication.java
@@ -20,7 +20,7 @@ public class PortfolioApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://frontendportfolio-5c98e.web.app/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://frontendportfolio-5c98e.web.app"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
